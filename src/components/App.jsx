2d56b714a85c13be118cf9ccb2911248d46fd652
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-30 17:00:30 -0700

    Ternary and And Operator

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,21 @@ import Login from "./Login";
 
 let isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+const currentTime = new Date().getHours();
+
+console.log(currentTime);
+
+//Ternary operator (?) is used as an if else statement. It checks if the condition before ?
+//is true. If so, it render the left hand side (expression) of the colon (:) otherwise it renders the right hand side (expression) of the colon
+
+//And operator (&&) is used to check if the condition on the left is true. If so, it render the expression after the && operator.
+//Otherwise it will do nothing if nothing is going to be rendered
 
 function App() {
-  return <div className="container"> {renderConditionally()}</div>;
+  return (
+    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    // currentTime > 12 && <h1>Why are you still working?</h1>
+  );
 }
 
 export default App;
